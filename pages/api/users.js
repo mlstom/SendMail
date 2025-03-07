8616feb6a2ffcc2128ecc36787f96f0c6d9dd42b
mlstom <93375537+mlstom@users.noreply.github.com>
2025-03-07 12:07:07 +0100

    Update users.js

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,8 +1,1 @@
-import database from "../../database"
 
-export default async function allUsers(req, res) {
-    const db = await database()
-    const [users] = await db.execute("SELECT * FROM User")
-    res.json(users)
-}
-  
